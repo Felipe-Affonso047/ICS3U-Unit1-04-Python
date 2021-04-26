54a968e17e0b2d8705ba8aa71473f11ffd9867f3
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-26 14:57:38 +0000

    simple math

---
+++ calculate_area_and_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Felipe Garcia Affonso
+# Created on: April 2021
+# This is the "calculate area and perimeter" program
+
+def main():
+    print("What is the area and perimeter of a 3 cm by 5 cm rectangle?")
+    print("area is:{}".format(3 * 5))
+    print("perimeter is:{}".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ simple_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Felipe Garcia Affonso
+# Created on: April 2021
+# This is the "simple math" program
+
+def main():
+    print("7+2={}".format(7 + 2))
+    print("6-9={}".format(6 - 9))
+    print("4*2={}".format(4 * 2))
+    print("9/3={}".format(9 / 3))
+    print("2**64={}".format(2 ** 64))
+
+
+if __name__ == "__main__":
+    main()
